19a4b3e75f73e516427d70becca5625f1949dfb8
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2022-06-01 22:54:27 +0300

    Add correct gcc for the MacOS build
    By default, gcc is symlinked to clang on MacOS. The build specifically
    calls the correct gcc executable now.

--- ci/build.sh
+++ ci/build.sh
@@ -1,6 +1,18 @@
 build_wiregrid() {
+
+    if [ "${OS_NAME}" = "macos-latest" ] && [ "${CC}" = "gcc" ]; then
+        # since the system's gcc is just a symlink to clang on macos,
+        # we have to specifically mention the path to it
+        #
+        # keep this in sync with the gcc version used
+        CC="/usr/local/bin/gcc-11"
+    fi
+
     echo "Running cmake.."
-    cmake -B build/
+    cmake -B build/ || exit
+
+    echo "Running make.."
+    make -C build/ || exit
 }
 
 build_wiregrid
